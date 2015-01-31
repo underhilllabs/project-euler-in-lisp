c121f2f09b38bb6796dda4dfda9c2f7908081678
Bart Lantz <underhilllabs@gmail.com>
2015-01-31 11:17:44 -0700

    Adds mult3or5p function

--- euler.lisp
+++ euler.lisp
@@ -14,19 +14,14 @@
   (=
    (ld n) n))
 
+(defun mult3or5p (n)
+  (cond ((= 0 (mod n 3)) t)
+        ((= 0 (mod n 5)) t)
+        (t nil)))
 
 
-
-
-
-
-
-
-
-
-
-
-
+;;(mod 3 13)
+;;(mult3or5p 13)
 ;; Testing
 ;;(divp 5 235)
 ;;(ld 45905909)
