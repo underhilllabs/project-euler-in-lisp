f8ec32e726875ce6344c0884e7b76e89e43727de
Bart Lantz <underhilllabs@gmail.com>
2015-02-18 13:43:54 -0700

    adds cheating prime factor code

--- pe.rb
+++ pe.rb
@@ -8,3 +8,9 @@ end
 def sum_mult_3_or_5(n)                                                                                    
  (1..n).select{|n| n % 3 == 0 or n % 5 == 0 }.reduce(:+)                                                  
 end        
+
+require 'mathn'
+# I had to work REALLY HARD for this answer!!
+def largest_prime_factor(n)
+   Prime.prime_division(n).last.first
+end
