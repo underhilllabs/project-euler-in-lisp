016b50dc7c72a659d4a16f96f7799a95dc359ede
Bart Lantz <underhilllabs@gmail.com>
2015-02-18 11:04:40 -0700

    Update pe.rb

--- pe.rb
+++ pe.rb
@@ -3,3 +3,8 @@
 def solve_20
   (1..100).reduce(:*).to_s.split("").reduce {|acc, num| acc.to_i + num.to_i}
 end
+
+# sum multiples of 3 or 5 upto n                                                                          
+def sum_mult_3_or_5(n)                                                                                    
+ (1..n).select{|n| n % 3 == 0 or n % 5 == 0 }.reduce(:+)                                                  
+end        
